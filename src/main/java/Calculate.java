5cfd83916de8d746303846aa0c640cc3381de5ce
wpwpwpwp <solworkk@gmail.com>
2022-10-18 08:53:12 +0300

    Калькулятор меню. v 2.0 поменял все по правкам

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -5,61 +5,85 @@ import java.util.Scanner;
 import javax.swing.JToolTip;
 
 public class Calculate {
-        float price;
-        int rubli;
-        String rublWord;
-        String productName;
-        float totalPrice;
-        String totalList = "";
-        String stopWord = "Завершить";
-        Scanner scanWords = new Scanner(System.in);
-        Scanner scanPrice = new Scanner(System.in);
-        float finalPrice;
-        public void calculate(int howPeople){
-            productName = scanWords.next();
-             while(!productName.equalsIgnoreCase(stopWord)){
-                 totalList=totalList+"\n"+productName;
-             System.out.println("Введите стоимость продукта");
-             price = scanPrice.nextFloat();
-             totalPrice=totalPrice+price;
-             System.out.println("Вы успешно добавили товар "+productName+" стоимостью: "+price+". Вы хотите добавить что-то ещё?");
-                 productName = scanWords.next();
+    float price;
+    int rubli;
+    String rublWord;
+    String productName;
+    float totalPrice;
+    String totalList = "";
+    String stopWord = "Завершить";
+    Scanner scanWords = new Scanner(System.in);
+    Scanner scanPrice = new Scanner(System.in);
+    float finalPrice;
+
+    public void calculate(int howPeople) {
+        productName = scanWords.next();
+        while (!productName.equalsIgnoreCase(stopWord)) {
+            totalList = totalList + "\n" + productName;
+            System.out.println("Введите стоимость продукта");
+            if (scanPrice.hasNextFloat() == false) {
+                System.out.println("Перезапустите программу и введите количество людей с помощью цифр");
+                break;
+            } else {
+                price = scanPrice.nextFloat();
+            }
+                totalPrice = totalPrice + price;
+                System.out.println("Вы успешно добавили товар " + productName + " стоимостью: " + price + ". Вы хотите добавить что-то ещё?");
+                productName = scanWords.next();
+                finalPrice = totalPrice / howPeople;
+                if ((int) finalPrice % 100 >= 11 && (int) finalPrice % 100 <= 14) {
+                    rubli = (int) finalPrice % 100;
+                    switch (rubli) {
+                        case 11:
+                            rublWord = "рублей";
+                            break;
+                        case 12:
+                            rublWord = "рублей";
+                            break;
+                        case 13:
+                            rublWord = "рублей";
+                            break;
+                        case 14:
+                            rublWord = "рублей";
+                            break;
+                    }
+                } else {
+                    rubli = (int) finalPrice % 10;
+                    switch (rubli) {
+                        case 1:
+                            rublWord = "рубль";
+                            break;
+                        case 2:
+                            rublWord = "рубля";
+                            break;
+                        case 3:
+                            rublWord = "рубля";
+                            break;
+                        case 4:
+                            rublWord = "рубля";
+                            break;
+                        case 5:
+                            rublWord = "рублей";
+                            break;
+                        case 6:
+                            rublWord = "рублей";
+                            break;
+                        case 7:
+                            rublWord = "рублей";
+                            break;
+                        case 8:
+                            rublWord = "рублей";
+                            break;
+                        case 9:
+                            rublWord = "рублей";
+                            break;
+                        case 0:
+                            rublWord = "рублей";
+                            break;
+                    }
+                }
         }
-        System.out.println("Добавленные товары: "+totalList+". Итоговая цена в рублях: "+totalPrice);
-             finalPrice = totalPrice / howPeople;
-             rubli =(int) finalPrice%10;
-             switch (rubli){
-                 case 1:
-                     rublWord = "рубль";
-                     break;
-                 case 2:
-                     rublWord = "рубля";
-                     break;
-                 case 3:
-                     rublWord = "рубля";
-                     break;
-                 case 4:
-                     rublWord = "рубля";
-                     break;
-                 case 5:
-                     rublWord = "рублей";
-                     break;
-                 case 6:
-                     rublWord = "рублей";
-                     break;
-                 case 7:
-                     rublWord = "рублей";
-                     break;
-                 case 8:
-                     rublWord = "рублей";
-                     break;
-                 case 9:
-                     rublWord = "рублей";
-                     break;
-                 case 0:
-                     rublWord = "рублей";
-                     break;
-             }
-             System.out.println("Каждый человек должен заплатить: " + String.format("%.2f", totalPrice)  + "" + rublWord);
-        }
+        System.out.println("Добавленные товары: " + totalList + "\n Итоговая цена в рублях: " + totalPrice);
+        System.out.println("Каждый человек должен заплатить: "+String.format("%.2f",finalPrice)+" "+rublWord); }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int howManyPeople = 1;
         Scanner scanner = new Scanner(System.in);
         Calculate calculator = new Calculate();
         System.out.println("На сколько человек необходимо разделить счет?");
-        int howManyPeople = scanner.nextInt();
-        while (true) {
-                 if (howManyPeople > 1) {
-                break;
-                 }
-                if (howManyPeople < 1) {
-                System.out.println("Вы ввели неверное количество человек, введите верное количество.");
-                howManyPeople = scanner.nextInt();
-                 } else {
-                System.out.println("Вы хотите поделить счет на одного человека, но зачем? Я понимаю, вы возможно одиноки, но смысла в этом нет. Введите верное количество людей.");
-                howManyPeople = scanner.nextInt();
-                }
+        if (scanner.hasNextInt() == false) {
+            System.out.println("Перезапустите программу и введите количество людей с помощью цифр");
         }
-        System.out.println("Какой товар Вы хотите добавить?");
-        calculator.calculate(howManyPeople);
+        else {
+            howManyPeople = scanner.nextInt();
+            while (true) {
+                if (howManyPeople > 1) {
+                    break;
+                }
+                if (howManyPeople < 1) {
+                    System.out.println("Вы ввели неверное количество человек, введите верное количество.");
+                    howManyPeople = scanner.nextInt();
+                } else {
+                    System.out.println("Вы хотите поделить счет на одного человека, но зачем? Я понимаю, вы возможно одиноки, но смысла в этом нет. Введите верное количество людей.");
+                    howManyPeople = scanner.nextInt();
+                }
+            }
+            System.out.println("Какой товар Вы хотите добавить?");
+            calculator.calculate(howManyPeople);
+        }
     }
 }
